757297b7b45ca74c2391752aef5e127f99a8b51b
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-02-14 08:01:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // write js code here if required
+setInterval(() => {
+	const now = new Date();
+	const day = now.getDate();
+	const month = now.getMonth() + 1;
+	const year = now.getFullYear();
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0');
+	
+	// Display the timer in the specified format
+	document.getElementById('timer').textContent = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+}, 1000)
